08060832212b309e6c3fdfab523ab16c3876e1cb
FalecidoGugu <gutigeovany@gmail.com>
2024-05-01 00:16:19 -0300

    Inicializando estrutura

---
+++ Contatos.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Contatos.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Contatos.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Contatos.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
